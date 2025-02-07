55d3ceb0cd81654c3f828189e760885a26680dd0
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2025-02-07 00:42:41 -0200

    Atualização arquivo index, css e Js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+    let res = document.getElementById('resp')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[Erro] Faltam Dados!')
