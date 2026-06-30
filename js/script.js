7ed1ef596f0caf1defad73dce8544beef32cc80f
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2026-06-29 21:10:17 -0300

    Atualização Estilo CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
+    let ini = document.getElementById('i_inic')
+    let fim = document.getElementById('i_fim')
+    let passo = document.getElementById('i_passo')
     let res = document.getElementById('resp')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
@@ -19,12 +19,12 @@ function contar() {
         // c < f Contagem Crescente
         if (i < f) {
             for (let c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{1F449} `
+                res.innerHTML += `${c} \u{1F449}`
             }
         } else {
             // c > f Contagem regressiva
             for (let c = i; c >= f; c -= p) {
-                res.innerHTML += ` ${c} \u{1F449} `    
+                res.innerHTML += `${c} \u{1F449}`    
             }
         }
         res.innerHTML += ` \u{1F3C1}`
